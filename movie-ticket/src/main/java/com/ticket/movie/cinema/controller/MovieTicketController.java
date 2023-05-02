a773359177b24a4aa0426826b0e0ac875408c3af
Esteban <esabinio13@gmail.com>
2023-05-02 09:52:01 -0300

    basic logic to buy transaction added

--- movie-ticket/src/main/java/com/ticket/movie/cinema/controller/MovieTicketController.java
+++ movie-ticket/src/main/java/com/ticket/movie/cinema/controller/MovieTicketController.java
@@ -1,9 +1,11 @@
 package com.ticket.movie.cinema.controller;
 
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import com.ticket.movie.cinema.model.MovieTicket;
 import com.ticket.movie.cinema.dto.MovieTicketDto;
 import com.ticket.movie.cinema.service.MovieTicketService;
 
@@ -25,10 +27,29 @@ public class MovieTicketController {
 
 
   @PostMapping("/{movieDateTheaterId}")
-  public Long  createNewTicket(@PathVariable("movieDateTheaterId") Long movieDateTheaterId){
+  public Integer  createNewTicket(@PathVariable("movieDateTheaterId") Long movieDateTheaterId){
     return this.movieTicketService.createNewTicket(movieDateTheaterId);
   }
 
+  @PostMapping("/id/{idTransaction}/seat/row/{row}/column/{column}")
+  public Integer  addSeatsToTicket(
+    @PathVariable("idTransaction") Integer idTransaction,
+    @PathVariable("row") Integer row,
+    @PathVariable("column") Integer column){
+    return this.movieTicketService.addSeatsToTicket(idTransaction,row,column);
+  }
+
+
+  @PostMapping("/pay/id/{idTransaction}")
+  public MovieTicket  payTicket(
+    @PathVariable("idTransaction") Integer idTransaction){
+    return this.movieTicketService.payTicket(idTransaction);
+  }
+
+  @GetMapping("/buylist")
+  public Map<Integer,MovieTicket> getAllListMovieTicket(){
+    return this.movieTicketService.getAllBuy();
+  }
   
  
 
--- movie-ticket/src/main/java/com/ticket/movie/cinema/service/MovieTicketService.java
+++ movie-ticket/src/main/java/com/ticket/movie/cinema/service/MovieTicketService.java
@@ -1,45 +1,78 @@
 package com.ticket.movie.cinema.service;
 
 
-import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ticket.movie.cinema.dto.MovieDateTheaterDto;
+//import com.ticket.movie.cinema.dto.MovieDateTheaterDto;
 import com.ticket.movie.cinema.dto.MovieTicketDto;
 import com.ticket.movie.cinema.model.MovieTicket;
 import com.ticket.movie.cinema.repository.MovieTicketRepository;
 
 
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+
+
 @Service
 public class MovieTicketService {
-  
+
+  Map<Integer,MovieTicket> buyList= new TreeMap<>();
+  Integer idTransaction = 1;
+
   @Autowired
   private MovieTicketRepository movieTicketRepository;
 
-  @Autowired
-  private MovieDateTheaterRestService movieDateTheaterRestService;
+  //@Autowired
+  //private MovieDateTheaterRestService movieDateTheaterRestService;
 
   public MovieTicketDto getMovieTicketDtoResponse(MovieTicket movieTicket){
-    MovieDateTheaterDto movieDateTheaterDto = this.movieDateTheaterRestService.getMovieDateTheaterById(movieTicket.getMovieDateTheaterId());
+    //MovieDateTheaterDto movieDateTheaterDto = this.movieDateTheaterRestService.getMovieDateTheaterById(movieTicket.getMovieDateTheaterId());
     return null;
 
   }
 
-  public Long createNewTicket(Long movieDateTheaterId) {
-    BuyTicketThread buy = new BuyTicketThread();
-
+  public Integer createNewTicket(Long movieDateTheaterId) {
     MovieTicket movieTicket = new MovieTicket();
     movieTicket.setMovieDateTheaterId(movieDateTheaterId);
 
-    Thread hiloBuy = new Thread (buy);
-    hiloBuy.start();
+    buyList.put(idTransaction,movieTicket);
     
+    this.deleteTransactionAfterTime(idTransaction);
 
-    hiloBuy.interrupt();
-    return null;
+    return idTransaction++; 
+  }
+
+  public Map<Integer,MovieTicket> getAllBuy() {
+    return this.buyList;
+  }
+
+  public Integer addSeatsToTicket(Integer idTransaction, Integer row, Integer column) {
+   buyList.get(idTransaction).setSeatRow(row);
+   buyList.get(idTransaction).setSeatColumn(column);
+  
+   return idTransaction;
+  }
+
+  public MovieTicket payTicket(Integer idTransaction) {
+   return this.movieTicketRepository.save(this.buyList.get(idTransaction));
+  }
+
+  public void deleteTransactionAfterTime(Integer idTransaction){
+    Timer timer = new Timer(100000, new ActionListener(){
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        buyList.remove(idTransaction);
+      }
+  });
+
+  timer.setRepeats(false);
+  timer.start();
   }
 
  
